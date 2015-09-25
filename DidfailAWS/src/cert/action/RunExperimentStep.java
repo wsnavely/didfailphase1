33b215b807ecb76e8dfe16d5fe69447ae7dcba5e
Will Snavely <wsnavely@sei.cmu.edu>
2015-09-25 21:28:58 +0100

    Small repo reorg + jvmMem option

--- DidfailAWS/src/cert/action/RunExperimentStep.java
+++ DidfailAWS/src/cert/action/RunExperimentStep.java
@@ -10,7 +10,7 @@ import cert.config.ExperimentConfig;
 
 public class RunExperimentStep extends SSHSessionStep {
 	private String experimentId;
-	private String jvmArgs;
+	private String jvmHeap;
 
 	public RunExperimentStep(AmazonEC2 ec2Conn, ExperimentConfig config) {
 		super(ec2Conn);
@@ -27,15 +27,16 @@ public class RunExperimentStep extends SSHSessionStep {
 		this.experimentId = experimentId;
 	}
 
-	public void setJvmArgs(String jvmArgs) {
-		this.jvmArgs = jvmArgs;
+	public void setJvmMem(String jvmHeap) {
+		this.jvmHeap = jvmHeap;
 	}
 
 	private String makeAnalysisCommand(MyInstanceInfo info) {
 		String dest = String.format("s3://flowdroidresults/%s/%s/", this.experimentId, info.id);
+		String script = "/home/ubuntu/flowdroid-runner/run.py";
 		String cd_cmd = "cd /home/ubuntu/flowdroid-runner";
 		String run_cmd = "nohup python %s --mem=\"%s\" --s3out=\"%s\" > /dev/null 2>&1";
-		run_cmd = String.format(run_cmd, "/home/ubuntu/flowdroid-runner/run.py", "20000", dest);
+		run_cmd = String.format(run_cmd, script, this.jvmHeap, dest);
 		return String.format("(%s; %s &)", cd_cmd, run_cmd);
 	}
 
@@ -48,7 +49,7 @@ public class RunExperimentStep extends SSHSessionStep {
 		RunExperimentStep step = new RunExperimentStep(conn, config);
 		step.setInstanceInfos(infos);
 		step.setExperimentId(config.experimentId);
-		//vmArgs(config.jvmArgs);
+		step.setJvmMem(config.javaHeapSize);
 		step.setLogin(config.login);
 		step.setKeyFile(config.privateKeyFile);
 		step.runAction();
